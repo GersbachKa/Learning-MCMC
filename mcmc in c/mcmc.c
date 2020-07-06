6919ae784c5541b859163d38e56e032dcf5eb962
Kyle Gersbach <39284048+GersbachKa@users.noreply.github.com>
2020-07-06 15:44:59 -0700

    Add files via upload

--- mcmc in c/mcmc.c
+++ mcmc in c/mcmc.c
@@ -4,12 +4,10 @@
 #include <math.h>
 #include <time.h>
 #define pi 3.14159265359
-#define chainLength 50000
 
 
 typedef struct{
-    double mu[chainLength];
-    double sigma[chainLength];
+    double *mu, *sigma;
     double acceptance;
     int size;
 }returnParams;
@@ -22,16 +20,12 @@ double boxMuller(void){
 }
 
 
-void randGaussian(double mu, double sigma, double * arr, int arr_size){
-    if(arr[0]==0){
-        for(int i=0;i<arr_size;i++){
-            arr[i]+=sigma*boxMuller()+mu;
-        }
-    }else{
-        for(int i=0;i<arr_size;i++){
-            arr[i]=sigma*boxMuller()+mu;
-        }
+double * randGaussian(double mu, double sigma, int arr_size){
+    double* arr = (double*)malloc(arr_size*sizeof(double));
+    for(int i=0;i<arr_size;i++){
+        arr[i]=sigma*boxMuller()+mu;
     }
+    return arr;
 }
 
 
@@ -43,31 +37,38 @@ double log_likelyhood(double * data, int data_size, double mu, double sigma){
     return ((double) data_size) * -(1)*log(sqrt(2*sigma*sigma)) + returnValue;
 }
 
+returnParams allocateMemory(int N){
+    returnParams output;
+    output.mu=(double*)malloc(N*sizeof(double));
+    output.sigma=(double*)malloc(N*sizeof(double));
+    output.size = N;
+    return output;
+}
 
-void mcmc(returnParams *output, double * data, int data_size, double jumpsize){
-    output->mu[0]=0.0;
-    output->sigma[0]=0.1;
-
+returnParams mcmc(double * data, int data_size, int N, double jumpsize){
+    returnParams output = allocateMemory(N);
+    output.mu[0]=0;
+    output.sigma[0]=0.1;
     int accept = 1;
-    for(int i=1;i<chainLength;i++){
-        double rando[2]; 
-        randGaussian(0,jumpsize,rando,2);
-        double newMu = output->mu[i-1] + rando[0];
-        double newSigma = output->sigma[i-1] + rando[1];
-        double oldLikely = log_likelyhood(data, data_size, output->mu[i-1],output->sigma[i-1]);
+    for(int i=1;i<N;i++){
+        double * rando = randGaussian(0,jumpsize,2);
+        double newMu = output.mu[i-1] + rando[0];
+        double newSigma = output.sigma[i-1] + rando[1];
+        free(rando);        
+        double oldLikely = log_likelyhood(data, data_size, output.mu[i-1],output.sigma[i-1]);
         double newLikely = log_likelyhood(data, data_size, newMu, newSigma);
         double logH = newLikely-oldLikely;
         if(logH>0 || logH>= log((double)rand()/RAND_MAX)){
-            output->mu[i] = newMu;
-            output->sigma[i] = newSigma;
+            output.mu[i] = newMu;
+            output.sigma[i] = newSigma;
             accept++;
         }else{
-            output->mu[i]=output->mu[i-1];
-            output->sigma[i]=output->sigma[i-1];
+            output.mu[i]=output.mu[i-1];
+            output.sigma[i]=output.sigma[i-1];
         }
     }
-    output->size = chainLength;
-    output->acceptance = (double) accept/chainLength;
+    output.acceptance = (double) accept/N;
+    return output;
 }
 
 
@@ -76,23 +77,20 @@ int main(){
     srand(time(0));
     
     printf("Generating gaussian.\n");
-    double data[10000] = {0};
-
-    
-    randGaussian(1.0,1.0,data,10000);
+    int data_size = 10000;
+    double * data = randGaussian(1.0,1.0,data_size);
     printf("Finished generating gaussian.\n");
 
     
     printf("Starting MCMC.\n");
-    returnParams out;
-    mcmc(&out, data, 10000, .005);
+    returnParams out = mcmc(data, data_size, 10000, .005);
     printf("Finished MCMC.\n");
     printf("Acceptance of: %f\n",out.acceptance);
     
 
     FILE *f;
     f = fopen("gaussian.txt","w");
-    for(int i=0;i<10000;i++){
+    for(int i=0;i<data_size;i++){
         fprintf(f,"%f\n",data[i]);
     }
     fclose(f);
@@ -104,6 +102,7 @@ int main(){
         fprintf(f1,"%f\t%f\n",out.mu[i],out.sigma[i]);
     }
     fclose(f1);
+    
     return 0;
     
 }
